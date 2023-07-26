e8986dcca48b2f60ed85c9754fb97d19659556b8
Enzo_Gatica <enzogatica96@gmail.com>
2023-07-26 19:23:23 -0400

    pendiente

---
+++ app/src/main/java/com/example/glidem5_i6/Usuario.java
@@ -0,0 +1,21 @@
+package com.example.glidem5_i6;
+
+public class Usuario {
+    private String nombre;
+    private String imagen;
+
+    public Usuario(String nombre, String imagen) {
+        this.nombre = nombre;
+        this.imagen = imagen;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+
+}
